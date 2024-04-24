8b0eb6f78b8211b90dfcfe345ec62bd5c5426451
Siva4440 <sivachakravarthi005@gmail.com>
2024-04-24 19:27:14 +0530

    chang

--- src/Routes/routes.js
+++ src/Routes/routes.js
@@ -1,17 +1,15 @@
 import React from "react";
-import { BrowserRouter, Routes, Route, createBrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "../Pages/Login/login";
 import Registration from "../Pages/Registration/registration";
 import Products from "../Pages/Products/products";
 import Header from "../Components/Header";
 import Profile from "../Pages/Profile/profile";
 
-let router = createBrowserRouter(Routes, { basename: "/shoppingCart" })
-
 const Router = () => {
   return (
     
-    <BrowserRouter basename={router}>
+    <BrowserRouter>
     < Header />
       <Routes>
           <Route path="/" element={<Login/>}></Route>
